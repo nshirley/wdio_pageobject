d0a00ada7473085913c859f424a1c6ee4b98396e
nshirley <nickshirley@mac.com>
2021-08-05 22:53:16 -0600

    remove reference to unused pages

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
-import Catalog from "./pageObjects/catalog.page";
 import LoginPage from "./pageObjects/login.page";
 import Page from "./pageObjects/page";
-import Program from "./pageObjects/program.page";
 import SecurePage from "./pageObjects/secure.page";
-import StudentHome from "./pageObjects/studentHome.page";
 
-export { LoginPage, StudentHome, Catalog, Program, Page, SecurePage };
+export { LoginPage, Page, SecurePage };
